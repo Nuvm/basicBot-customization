631636f9eae6d1458c92c3f654ee9707026a1646
Nuvm <naokimc1@gmail.com>
2015-01-15 20:37:39 -0500

    Update extension.js
    added smoke command

--- extension.js
+++ extension.js
@@ -32,15 +32,15 @@
 
          */
 
-        bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
+        bot.commands.smokeCommand = {
+            command: 'smoke',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'host', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat(":smoke: Hatsune is the best! :smoke:");
                 }
             }
         };
